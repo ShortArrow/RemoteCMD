7524fd3718a9177deeed91dba5c87abedba90131
ShortArrow <bamboogeneral@live.jp>
2021-10-11 08:43:55 +0900

    first commit

---
+++ RemoteCMD/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace RemoteCMD {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxTargetHost = new System.Windows.Forms.TextBox();
+            this.textBoxIPMR = new System.Windows.Forms.TextBox();
+            this.textBoxIPML = new System.Windows.Forms.TextBox();
+            this.textBoxIPL = new System.Windows.Forms.TextBox();
+            this.textBoxIPR = new System.Windows.Forms.TextBox();
+            this.buttonMake = new System.Windows.Forms.Button();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxTargetHost
+            // 
+            this.textBoxTargetHost.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxTargetHost.Location = new System.Drawing.Point(12, 129);
+            this.textBoxTargetHost.Name = "textBoxTargetHost";
+            this.textBoxTargetHost.Size = new System.Drawing.Size(242, 23);
+            this.textBoxTargetHost.TabIndex = 6;
+            this.textBoxTargetHost.TextChanged += new System.EventHandler(this.HostNameChanged);
+            this.textBoxTargetHost.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxEnterKey);
+            // 
+            // textBoxIPMR
+            // 
+            this.textBoxIPMR.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxIPMR.Location = new System.Drawing.Point(198, 85);
+            this.textBoxIPMR.Name = "textBoxIPMR";
+            this.textBoxIPMR.Size = new System.Drawing.Size(56, 23);
+            this.textBoxIPMR.TabIndex = 5;
+            this.textBoxIPMR.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxIPMR.TextChanged += new System.EventHandler(this.ChangedIP);
+            this.textBoxIPMR.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxEnterKey);
+            // 
+            // textBoxIPML
+            // 
+            this.textBoxIPML.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxIPML.Location = new System.Drawing.Point(12, 85);
+            this.textBoxIPML.Name = "textBoxIPML";
+            this.textBoxIPML.Size = new System.Drawing.Size(56, 23);
+            this.textBoxIPML.TabIndex = 2;
+            this.textBoxIPML.Text = "192";
+            this.textBoxIPML.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxIPML.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxEnterKey);
+            // 
+            // textBoxIPL
+            // 
+            this.textBoxIPL.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxIPL.Location = new System.Drawing.Point(74, 85);
+            this.textBoxIPL.Name = "textBoxIPL";
+            this.textBoxIPL.Size = new System.Drawing.Size(56, 23);
+            this.textBoxIPL.TabIndex = 3;
+            this.textBoxIPL.Text = "168";
+            this.textBoxIPL.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxIPL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxEnterKey);
+            // 
+            // textBoxIPR
+            // 
+            this.textBoxIPR.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxIPR.Location = new System.Drawing.Point(136, 85);
+            this.textBoxIPR.Name = "textBoxIPR";
+            this.textBoxIPR.Size = new System.Drawing.Size(56, 23);
+            this.textBoxIPR.TabIndex = 4;
+            this.textBoxIPR.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxIPR.TextChanged += new System.EventHandler(this.ChangedIP);
+            this.textBoxIPR.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxEnterKey);
+            // 
+            // buttonMake
+            // 
+            this.buttonMake.Font = new System.Drawing.Font("Meiryo UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonMake.Location = new System.Drawing.Point(12, 12);
+            this.buttonMake.Name = "buttonMake";
+            this.buttonMake.Size = new System.Drawing.Size(242, 67);
+            this.buttonMake.TabIndex = 1;
+            this.buttonMake.Text = "MAKE";
+            this.buttonMake.UseVisualStyleBackColor = true;
+            this.buttonMake.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxUserName.Location = new System.Drawing.Point(12, 173);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(242, 23);
+            this.textBoxUserName.TabIndex = 7;
+            this.textBoxUserName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxEnterKey);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxPassword.Location = new System.Drawing.Point(12, 217);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(242, 23);
+            this.textBoxPassword.TabIndex = 8;
+            this.textBoxPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxEnterKey);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HostName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "UserName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(12, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 255);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.buttonMake);
+            this.Controls.Add(this.textBoxIPR);
+            this.Controls.Add(this.textBoxIPL);
+            this.Controls.Add(this.textBoxIPML);
+            this.Controls.Add(this.textBoxIPMR);
+            this.Controls.Add(this.textBoxTargetHost);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RemoteCMD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTargetHost;
+        private System.Windows.Forms.TextBox textBoxIPMR;
+        private System.Windows.Forms.TextBox textBoxIPML;
+        private System.Windows.Forms.TextBox textBoxIPL;
+        private System.Windows.Forms.TextBox textBoxIPR;
+        private System.Windows.Forms.Button buttonMake;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RemoteCMD/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RemoteCMD {
+    public partial class Form1 : Form {
+        private Encoding Enc
+        {
+            get
+            {
+                return Encoding.GetEncoding("SHIFT_JIS");
+            }
+        }
+        private List<byte> targetIP = new List<byte>();
+        private List<TextBox> textboxIP = new List<TextBox>();
+        private string targetHostName = null;
+        private string tagetUserName = null;
+        private string targetPassword = null;
+        private string accessTarget = null;
+        public Form1() {
+            InitializeComponent();
+            targetIP.Add(0);
+            targetIP.Add(0);
+            targetIP.Add(0);
+            targetIP.Add(0);
+            textboxIP.Add(textBoxIPML);
+            textboxIP.Add(textBoxIPL);
+            textboxIP.Add(textBoxIPR);
+            textboxIP.Add(textBoxIPMR);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            //入力処理
+            byte ans;
+            if (textBoxIPMR.Enabled) {
+                foreach (var i in Enumerable.Range(0, textboxIP.Count)) {
+                    if (byte.TryParse(textboxIP[i].Text, out ans)) {
+                        targetIP[i] = ans;
+                    }
+                    else if(textboxIP[i].Text == ""){
+                        MessageBox.Show("please input IP or HostName");
+                        return;
+                    }
+                    else {
+                        Console.WriteLine("INPUT ERROR : IP NOT EXISTS");
+                        MessageBox.Show("please input 0~255");
+                        return;
+                    }
+                }
+                accessTarget = targetIP[0].ToString() + "."+targetIP[1].ToString() + "." + targetIP[2].ToString() + "." + targetIP[3].ToString();
+            }
+            if (textBoxTargetHost.Enabled) {
+                if (targetHostName=="") {
+                    MessageBox.Show("please input hostname");
+                    return;
+                }
+                targetHostName = textBoxTargetHost.Text;
+                accessTarget = targetHostName;
+            }
+            tagetUserName = textBoxUserName.Text;
+            if (tagetUserName=="") {
+                MessageBox.Show("please input username");
+                return;
+            }
+            targetPassword = textBoxPassword.Text;
+            if (targetPassword=="") {
+                MessageBox.Show("please input password");
+                return;
+            }
+            //入力拒否
+            buttonMake.Enabled = false;
+
+            //デスクトップを探す
+            string desktopDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            //一時ファイルを作成する
+            string tmpPath = Path.GetTempFileName();
+            //一時ファイルを書き込みで開く
+            StreamWriter sw = new StreamWriter(tmpPath, false, Enc);
+            //内容を一行ずつ読み込む
+            sw.Write(@"@psexec -u " + accessTarget + @"\" + tagetUserName + @" -p " + targetPassword + @" \\" + accessTarget + @" cmd");
+            //閉じる
+            sw.Close();
+            //一時ファイルと入れ替える
+            File.Copy(tmpPath, desktopDirectoryPath + @"\" + accessTarget + ".cmd", true);
+            File.Delete(tmpPath);
+            MessageBox.Show("Make this file on Desktop\n<" + accessTarget + ".cmd>");
+            //入力許可
+            buttonMake.Enabled = true;
+        }
+
+        private void HostNameChanged(object sender, EventArgs e) {
+            bool canUseIP;
+            if (textBoxTargetHost.Text == "") {
+                canUseIP = true;
+            }
+            else {
+                canUseIP = false;
+            }
+            foreach (var i in Enumerable.Range(0, textboxIP.Count)) {
+                textboxIP[i].Enabled = canUseIP;
+            }
+        }
+        private void ChangedIP(object sender, EventArgs e) {
+            bool canUseHostName=true;
+            if (textboxIP[0].Text != "" & textboxIP[1].Text != "" & textboxIP[2].Text != "" & textboxIP[3].Text != "") {
+                canUseHostName = false;
+            }
+            textBoxTargetHost.Enabled = canUseHostName;
+        }
+        private void TextBoxEnterKey(object sender, KeyEventArgs e) {
+          if (e.KeyData==Keys.Enter) {
+                buttonMake.PerformClick();
+            }
+        }
+    }
+}
